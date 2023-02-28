b07b52dfb6c6b4de10ed0e3d4b12ff45dc5eead5
Salaxer1981 <alexandercedeno@gmail.com>
2023-02-27 23:25:32 -0300

    Primer Commit

---
+++ src/controllers/user.controllers.js
@@ -0,0 +1,54 @@
+const catchError = require('../utils/catchError');
+const User = require('../models/User');
+const { where } = require('sequelize');
+
+const getAll = catchError(async (req, res) => {
+
+    const users = await User.findAll();
+
+    return res.json(users)
+});
+
+const create = catchError(async (req, res) => {
+
+    const { first_name, last_name, email, password, birthday } = req.body;
+
+    const user = await User.create({ first_name, last_name, email, password, birthday })
+
+    return res.status(201).json(user)
+});
+
+const getOne = catchError(async(req, res) => {
+    const { id } = req.params
+    const user = await User.findByPk(id);
+    if(!user) return res.status(404).json({message: "User not found"})
+    return res.json(user)
+
+
+})
+
+const remove = catchError(async (req, res) => {
+    const { id } = req.params
+    const user = await User.destroy({ where: { id } })
+    if (user === 0) return res.status(404).json({ message: "User not found" })
+    return res.sendStatus(204);
+});
+
+const update = catchError(async (req, res) => {
+
+    const { first_name, last_name, email, password, birthday } = req.body;
+    const { id } = req.params
+    const user = await User.update(
+        { first_name, last_name, email, password, birthday },
+        { where: { id }, returning: true }
+    )
+    return res.json(user[1],[0]);
+})
+
+module.exports = {
+    getAll,
+    create,
+    remove,
+    update,
+    getOne
+}
